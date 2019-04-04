a88b9bfa30fbe2be78b29f0aabf47edbdb342abe
dragoonvignesh1998 <dragoonvignesh1998@gmail.com>
2019-04-04 20:03:18 +0530

    test6 file with errors

---
+++ og-proj/test6error.lua
@@ -0,0 +1,19 @@
+function fibonacci(n)
+    if n<3 then
+        return 1
+    else
+        return fibonacci(n-1) + fibonacci(n-2)
+    end
+end
+function fibonacci1
+    if n<3 then
+        return 1
+    else
+        return fibonacci1(n-1) + fibonacci(n-2)
+    end
+end
+
+for n = 1, 16 do
+    print(fibonacci(n))
+end
+io.write("...\n")
